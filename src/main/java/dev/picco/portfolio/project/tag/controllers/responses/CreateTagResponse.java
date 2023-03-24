8b690ecf2af295e5013116bf70db9cfcb124ddb6
Rafiere <rafapgbr@gmail.com>
2023-03-24 08:54:19 -0300

    feat: add create tag feature components

--- src/main/java/dev/picco/portfolio/project/tag/controllers/responses/CreateTagResponse.java
+++ src/main/java/dev/picco/portfolio/project/tag/controllers/responses/CreateTagResponse.java
@@ -1,8 +1,8 @@
 package dev.picco.portfolio.project.tag.controllers.responses;
 
-public record CreateTagResponse(Long tagId, String tagName) {
+public record CreateTagResponse(Long tagId, String tagName, String tagColor) {
 
-	public static CreateTagResponse of(final Long tagId, final String tagName){
-		return new CreateTagResponse(tagId, tagName);
+	public static CreateTagResponse of(final Long tagId, final String tagName, final String tagColor){
+		return new CreateTagResponse(tagId, tagName, tagColor);
 	}
 }
