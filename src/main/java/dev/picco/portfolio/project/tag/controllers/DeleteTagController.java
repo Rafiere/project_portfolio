9d9e41f79f2c606976f75451fe5eed57d70db2e9
Rafiere <rafapgbr@gmail.com>
2023-03-24 08:54:57 -0300

    feat: add delete tag feature components

--- src/main/java/dev/picco/portfolio/project/tag/controllers/DeleteTagController.java
+++ src/main/java/dev/picco/portfolio/project/tag/controllers/DeleteTagController.java
@@ -1,5 +1,24 @@
 package dev.picco.portfolio.project.tag.controllers;
 
+import dev.picco.portfolio.project.tag.services.DeleteTagService;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class DeleteTagController {
 
+	private final DeleteTagService deleteTagService;
+
+	@DeleteMapping("/v1/tag/{tagId}")
+	public ResponseEntity<Void> execute(@NotNull @PathVariable final Long tagId){
+
+		deleteTagService.execute(tagId);
+
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/dev/picco/portfolio/project/tag/services/DeleteTagService.java
+++ src/main/java/dev/picco/portfolio/project/tag/services/DeleteTagService.java
@@ -1,5 +1,17 @@
 package dev.picco.portfolio.project.tag.services;
 
+import dev.picco.portfolio.project.tag.repositories.TagRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class DeleteTagService {
 
+	private final TagRepository tagRepository;
+
+	public void execute(final Long tagId) {
+
+		tagRepository.deleteById(tagId);
+	}
 }
