3a1e33bf1ff686e1dd62f6714f192dc0074bc3be
Rafiere <rafapgbr@gmail.com>
2023-03-24 08:54:16 -0300

    feat: add create tag feature components

--- src/main/java/dev/picco/portfolio/project/tag/controllers/requests/CreateTagRequest.java
+++ src/main/java/dev/picco/portfolio/project/tag/controllers/requests/CreateTagRequest.java
@@ -2,6 +2,6 @@ package dev.picco.portfolio.project.tag.controllers.requests;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record CreateTagRequest(@NotBlank String tagName) {
+public record CreateTagRequest(@NotBlank String name, @NotBlank String color) {
 
 }
