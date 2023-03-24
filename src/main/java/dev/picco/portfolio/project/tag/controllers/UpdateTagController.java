430fcfe0017330140724690c41fc620353c3a1a9
Rafiere <rafapgbr@gmail.com>
2023-03-24 09:05:18 -0300

    feat: add update tag components

--- src/main/java/dev/picco/portfolio/project/tag/controllers/UpdateTagController.java
+++ src/main/java/dev/picco/portfolio/project/tag/controllers/UpdateTagController.java
@@ -1,5 +1,31 @@
 package dev.picco.portfolio.project.tag.controllers;
 
+import dev.picco.portfolio.project.tag.controllers.requests.CreateTagRequest;
+import dev.picco.portfolio.project.tag.controllers.responses.CreateTagResponse;
+import dev.picco.portfolio.project.tag.services.UpdateTagService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class UpdateTagController {
 
+	private final UpdateTagService updateTagService;
+
+	@PatchMapping("/v1/tag/{tagId}")
+	public ResponseEntity<CreateTagResponse> execute(@PathVariable @NotBlank final Long tagId,
+	                                                 @Valid @RequestBody @NotNull final CreateTagRequest request) {
+
+		var response = updateTagService.execute(tagId, request);
+
+		return ResponseEntity.status(201)
+		                     .body(response);
+	}
 }
--- src/main/java/dev/picco/portfolio/project/tag/services/UpdateTagService.java
+++ src/main/java/dev/picco/portfolio/project/tag/services/UpdateTagService.java
@@ -1,5 +1,25 @@
 package dev.picco.portfolio.project.tag.services;
 
+import dev.picco.portfolio.project.tag.controllers.requests.CreateTagRequest;
+import dev.picco.portfolio.project.tag.controllers.responses.CreateTagResponse;
+import dev.picco.portfolio.project.tag.domain.Tag;
+import dev.picco.portfolio.project.tag.repositories.TagRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+@RequiredArgsConstructor
 public class UpdateTagService {
 
+	private final TagRepository tagRepository;
+
+	public CreateTagResponse execute(final Long tagId, final CreateTagRequest request) {
+
+		Tag tag = tagRepository.findById(tagId).orElseThrow(() -> new IllegalArgumentException("A tag with ID " + tagId + " does not exist."));
+
+		tag.update(request.name(), request.color());
+
+		return CreateTagResponse.of(tag.getId(), tag.getName(), tag.getBackgroundColor());
+	}
 }
