2ceef28a279863feb1754fa0d79fd7b4bd412520
Rafiere <rafapgbr@gmail.com>
2023-03-24 08:54:44 -0300

    feat: add create tag feature components

--- src/main/java/dev/picco/portfolio/project/tag/services/CreateTagService.java
+++ src/main/java/dev/picco/portfolio/project/tag/services/CreateTagService.java
@@ -2,11 +2,23 @@ package dev.picco.portfolio.project.tag.services;
 
 import dev.picco.portfolio.project.tag.controllers.requests.CreateTagRequest;
 import dev.picco.portfolio.project.tag.controllers.responses.CreateTagResponse;
+import dev.picco.portfolio.project.tag.domain.Tag;
+import dev.picco.portfolio.project.tag.repositories.TagRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class CreateTagService {
 
-	public CreateTagResponse execute(CreateTagRequest request) {
+	private final TagRepository tagRepository;
 
-		return CreateTagResponse.of(1L, "teste");
+	public CreateTagResponse execute(final CreateTagRequest request) {
+
+		Tag createdTag = Tag.of(request.name(), request.color());
+
+		Tag savedTag = tagRepository.save(createdTag);
+
+		return CreateTagResponse.of(savedTag.getId(), savedTag.getName(), savedTag.getBackgroundColor());
 	}
 }
