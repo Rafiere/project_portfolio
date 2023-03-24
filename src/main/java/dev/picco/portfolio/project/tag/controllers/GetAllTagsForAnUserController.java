ed92a6a7026ce670b0b9db7b6b10388578cbe310
Rafiere <rafapgbr@gmail.com>
2023-03-24 09:21:45 -0300

    feat: add get all tags components

--- src/main/java/dev/picco/portfolio/project/tag/controllers/GetAllTagsForAnUserController.java
+++ src/main/java/dev/picco/portfolio/project/tag/controllers/GetAllTagsForAnUserController.java
@@ -1,5 +1,26 @@
 package dev.picco.portfolio.project.tag.controllers;
 
+import dev.picco.portfolio.project.tag.controllers.responses.GetTagByIdResponse;
+import dev.picco.portfolio.project.tag.services.GetAllTagsForAnUserService;
+import dev.picco.portfolio.project.tag.services.GetTagByIdService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class GetAllTagsForAnUserController {
 
+	private final GetAllTagsForAnUserService getAllTagsForAnUserService;
+
+	@GetMapping("/v1/tags")
+	public ResponseEntity<List<GetTagByIdResponse>> execute(){
+
+		var response = getAllTagsForAnUserService.execute();
+
+		return ResponseEntity.ok(response);
+	}
 }
--- src/main/java/dev/picco/portfolio/project/tag/services/GetAllTagsForAnUserService.java
+++ src/main/java/dev/picco/portfolio/project/tag/services/GetAllTagsForAnUserService.java
@@ -1,13 +1,25 @@
 package dev.picco.portfolio.project.tag.services;
 
+import dev.picco.portfolio.project.tag.controllers.responses.GetTagByIdResponse;
+import dev.picco.portfolio.project.tag.domain.Tag;
+import dev.picco.portfolio.project.tag.repositories.TagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class GetAllTagsForAnUserService {
 
-	public void execute(){
+	private final TagRepository tagRepository;
 
+	public List<GetTagByIdResponse> execute() {
+
+		List<Tag> tags = tagRepository.findAll();
+
+		return tags.stream()
+		           .map(tag -> GetTagByIdResponse.of(tag.getId(), tag.getName(), tag.getBackgroundColor()))
+		           .toList();
 	}
 }
