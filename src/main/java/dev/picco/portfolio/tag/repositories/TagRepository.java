b4dcaaadf83647fa08a0bd5385944055039cb62a
Rafiere <rafapgbr@gmail.com>
2023-03-26 19:36:48 -0300

    feat: add query to verify missing tag ids in database

--- src/main/java/dev/picco/portfolio/tag/repositories/TagRepository.java
+++ src/main/java/dev/picco/portfolio/tag/repositories/TagRepository.java
@@ -2,7 +2,14 @@ package dev.picco.portfolio.tag.repositories;
 
 import dev.picco.portfolio.tag.domain.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
+	@Query("SELECT t FROM Tag t " +
+	       "WHERE t.id IN :tagsIds " +
+	       "AND NOT EXISTS (SELECT 1 FROM Tag t2 WHERE t2.id = t.id)")
+	List<Long> findMissingTagIds(List<Long> tagsIds);
 }
